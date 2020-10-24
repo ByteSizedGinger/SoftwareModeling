81ae2f8c6b444d8597dbd87ee73d7224c4d717ee
Michael Harvey <39992590+MichaelJHarvey@users.noreply.github.com>
2020-10-24 20:02:34 +0200

    fix

---
+++ Car/Parts/Aerodynamics.h
@@ -0,0 +1,16 @@
+#ifndef AERODYNAMICS_H
+#define AERODYNAMICS_H
+
+#include <string>
+#include "Car/Car.h"
+
+using namespace std;
+
+class Aerodynamics {
+
+public:
+    Aerodynamics(Car* c);
+
+};
+
+#endif
