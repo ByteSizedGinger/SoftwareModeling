1c15c01a0f2c622c17e627d2b7babf86eb066f63
Jenna Gallagher <u19025638@tuks.co.za>
2020-10-24 20:05:05 +0200

    Chasis Electronics header files

---
+++ Car/Parts/Chassis.h
@@ -0,0 +1,16 @@
+#ifndef CHASIS_H
+#define CHASIS_H
+
+#include <string>
+#include "Car/Car.h"
+
+using namespace std;
+
+class Chasis {
+
+public:
+    Chasis(Car* c);
+
+};
+
+#endif
---
+++ Car/Parts/Electronics.h
@@ -0,0 +1,16 @@
+#ifndef Electronics_H
+#define Electronics_H
+
+#include <string>
+#include "Car/Car.h"
+
+using namespace std;
+
+class Electronics {
+
+public:
+    Electronics(Car* c);
+
+};
+
+#endif
