52a4e0463f5624b422aa42b339fd0e13d901e00d
Wessel Kruger <72206668+Wesselk21@users.noreply.github.com>
2020-11-04 13:09:06 +0200

    Update Electronics.cpp

--- Car/Parts/Electronics.cpp
+++ Car/Parts/Electronics.cpp
@@ -7,6 +7,13 @@ Electronics::Electronics(){
     type = "Electronics";
 }
 
+Electronics* Electronics::cpy() { //might have to make return type Car*
+	Electronics* temp = new Electronics();
+	temp.speed = speed;
+	temp->next = nullptr;
+	return temp;
+}
+
 void Electronics::removePart(string part) {
     if (part == this->getType()) cout << part << " has been removed." << endl;
     else DepartmentOutput::removePart(part);
@@ -14,4 +21,13 @@ void Electronics::removePart(string part) {
 
 double Electronics::getSpeed(){
     return speed * 10;
+}
+
+Car* Electronics::clone() {
+	Car* temp = cpy();
+	if (next != nullptr)
+	{
+		temp->next = next->clone();
+	}
+	return temp;
 }
