7e22820033ee05d459aa8e4e7798715dfb869ce5
Wessel Kruger <72206668+Wesselk21@users.noreply.github.com>
2020-11-04 13:08:39 +0200

    Update Aerodynamics.cpp

--- Car/Parts/Aerodynamics.cpp
+++ Car/Parts/Aerodynamics.cpp
@@ -7,6 +7,13 @@ Aerodynamics::Aerodynamics(){
     type = "Aerodynamics";
 }
 
+Aerodynamics* Aerodynamics::cpy() { //might have to make return type Car*
+	Aerodynamics* temp = new Aerodynamics();
+	temp.speed = speed;
+	temp->next = nullptr;
+	return temp;
+}
+
 void Aerodynamics::removePart(string part) {
     if (part == this->getType()) cout << part << " has been removed." << endl;
     else DepartmentOutput::removePart(part);
@@ -14,4 +21,13 @@ void Aerodynamics::removePart(string part) {
 
 double Aerodynamics::getSpeed(){
     return speed * 1000;
+}
+
+Car* Aerodynamics::clone() {
+	Car* temp = cpy();
+	if (next != nullptr)
+	{
+		temp->next = next->clone();
+	}
+	return temp;
 }
