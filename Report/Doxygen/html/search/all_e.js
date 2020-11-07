8536fb73154048a364956fd62df6368ec36bb67b
Michael Harvey <39992590+MichaelJHarvey@users.noreply.github.com>
2020-11-07 19:59:46 +0200

    refix factory

---
+++ Report/Doxygen/html/search/all_e.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['team',['Team',['../classTeam.html',1,'Team'],['../classTeam.html#acb3e8e4707763829d17d70b26deaee34',1,'Team::Team()']]],
+  ['transport',['transport',['../classShip.html#a8b60ca34490dd8ef1c93888fa61c9472',1,'Ship::transport()'],['../classTransportStrategy.html#ab5659f9d5d352ba0a23f1e99edd3d488',1,'TransportStrategy::transport()'],['../classTruck.html#a260ad20acea80d5b4faa2a9d97eaa8e9',1,'Truck::transport()']]],
+  ['transportstrategy',['TransportStrategy',['../classTransportStrategy.html',1,'']]],
+  ['truck',['Truck',['../classTruck.html',1,'']]],
+  ['tyre',['Tyre',['../classTyre.html',1,'']]]
+];
---
+++ Report/Doxygen/html/search/functions_c.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['setdriver',['setDriver',['../classCar.html#ad7148bd1abe107ce2e2b134f78582c9d',1,'Car::setDriver()'],['../classCurrentSeason.html#a3ea9de8713c6d2c21202351b036af75f',1,'CurrentSeason::setDriver()'],['../classDepartmentOutput.html#aa045362e5763d57a2c888b98c677773c',1,'DepartmentOutput::setDriver()']]],
+  ['setisbox',['setIsBox',['../classContainer.html#a9bba4330d6f63a7696f6932819177cac',1,'Container']]],
+  ['setperformance',['setPerformance',['../classDriver.html#aa8e4524d765561c16d2bf6072e963e83',1,'Driver']]],
+  ['setpoints',['setPoints',['../classDriver.html#a10b1b27d8d99ed19304b69727f0a0504',1,'Driver']]],
+  ['setracetime',['setRaceTime',['../classCurrentSeason.html#aae01cbd64006ce4fe60ea337b4f02962',1,'CurrentSeason::setRaceTime()'],['../classDepartmentOutput.html#a99c1b2cf729b25bc2f1d48214019c678',1,'DepartmentOutput::setRaceTime()']]],
+  ['simulate',['simulate',['../classCurrentSeason.html#a4828aaa2c0b914b55ebb34c0d1062e8e',1,'CurrentSeason']]],
+  ['soft',['Soft',['../classSoft.html#afd05f47d2416be24209308d448ec2979',1,'Soft']]]
+];
